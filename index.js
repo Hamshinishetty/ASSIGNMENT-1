1d724af32ed2d00e79e60b83952d904a12cd923f
Hamshini <hamshini11203@gmail.com>
2024-02-17 20:38:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import authRouter from './routers/auth.router.js'
+import blogRouter from './routers/blog.router.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/auth", authRouter)
+app.use("/blog", blogRouter)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("server started running at the PORT:", PORT))
